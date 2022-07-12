189965f875d2a71996a52d44afef673b7bdf20dd
zaridem <zarinalitk@gmail.com>
2022-07-12 16:09:09 +0300

    Added relevant icon from the API

--- src/index.js
+++ src/index.js
@@ -35,6 +35,13 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 function searchCity(city) {
